3b0c45233e1f99caf5ae4c6fd9e1f7bb7ed73541
Indumathi-V <54302074+Indumathi-V@users.noreply.github.com>
2026-08-08 14:26:49 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+  // TODO 1:
+  // Create a state variable to store login status.
+  // Initial value should be false.
+
+
+  // TODO 2:
+  // Create a Login button.
+  // Use onClick to change the login status.
+
+
+  // TODO 3:
+  // Use a ternary operator to conditionally display:
+  //
+  // "Welcome Student" when logged in
+  //
+  // "Login" when not logged in.
+
+
+}
+
+export default App;
